b3c762eecc7bc0d037d91ca451c7a57ef9f6079d
thumaraujo <165036906+thumaraujo@users.noreply.github.com>
2024-10-18 17:30:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ export default function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Search />
+      This project was coded by Thuany and can be found on{" "}
+      <a
+        href="https://github.com/thumaraujo/react-homework4"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        Github
+      </a>
+      .
     </div>
   );
 }
